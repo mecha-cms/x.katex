e8a85b79a051017d56c35f64a8743f69b65e78e9
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-02-11 10:38:38 +0700

    Build Tool

--- index/panel.php
+++ index/panel.php
@@ -13,40 +13,9 @@ Hook::set('_', function ($_) {
         if (!isset($_['asset'][$key])) {
             continue;
         }
+        // Do not prevent KaTeX from being loaded into the Panel
+        $_['asset'][$key]['id'] = false;
         $_['asset'][$key]['skip'] = false;
-        $_['asset'][$key]['stack'] = 20;
     }
-    $script = <<<JS
-(function (w, d) {
-    function onChange() {
-        if (!w.katex) return;
-        let nodes = d.querySelectorAll('samp.katex,samp.katex-display');
-        nodes.length && nodes.forEach(node => {
-            w.katex.render(node.textContent, node, {
-                displayMode: node.classList.contains('katex-display'),
-                throwOnError: false
-            });
-            node.replaceWith(...node.childNodes);
-        });
-    }
-    if (w._) {
-        w._.on('change', onChange);
-        w._.on('set', onChange);
-    }
-})(window, document);
-JS;
-    $style = <<<CSS
-.katex-display {
-  font-size: 150%;
-}
-CSS;
-    $_['asset']['katex:script'] = [
-        'link' => 'data:text/js;base64,' . To::base64($script),
-        'stack' => 20.1
-    ];
-    $_['asset']['katex:style'] = [
-        'link' => 'data:text/css;base64,' . To::base64($style),
-        'stack' => 20.1
-    ];
     return $_;
 }, 0);
